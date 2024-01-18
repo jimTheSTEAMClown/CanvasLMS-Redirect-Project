77da1b1c2bd2406960133320065519bfd0d6625a
Jim Burnham <jim.the.steam.clown@gmail.com>
2024-01-18 05:44:14 -0800

    Create canvasLMS-URL-Redirect.php

---
+++ canvasLMS-URL-Redirect.php
@@ -0,0 +1,19 @@
+<?php
+
+// Check if the 'text' parameter is present in the URL
+if(isset($_GET['text'])) {
+    // Get the value of the 'text' parameter
+    $text = $_GET['text'];
+    
+    // Replace <class_code> with 1234 in the text string
+    $modifiedText = str_replace('<class_code>', '1243', $text);
+    
+    // Redirect to the modified URL
+    header("Location: $modifiedText");
+    exit; // Ensure that no further code is executed after the redirect
+} else {
+    // If 'text' parameter is not present, print an error message
+    echo "Error: 'text' parameter is missing in the URL.";
+}
+
+?>
